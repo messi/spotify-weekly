d9ba600607b2278af013ea8c5119efbc9eaa4dc9
Philip Messlehner <mess.philip@gmail.com>
2019-05-12 10:30:24 -0700

    implemented first version of weekly archive

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,61 @@
 import * as functions from 'firebase-functions';
+const SpotifyWebApi = require('spotify-web-api-node');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const spotifyApi = new SpotifyWebApi({
+    clientId: functions.config().weeklyarchive.client_id,
+    clientSecret: functions.config().weeklyarchive.client_secret,
+    redirectUri: functions.config().weeklyarchive.redirect_url
+});
+
+spotifyApi.setRefreshToken(functions.config().weeklyarchive.refresh_token);
+
+exports.weeklyArchive = functions.pubsub.schedule('every 12 hours').onRun((context) => {
+    spotifyApi.refreshAccessToken()
+    .then(function(accessTokenData: any) {
+        console.log('The access token has been refreshed!');
+        spotifyApi.setAccessToken(accessTokenData.body['access_token']);
+        
+        spotifyApi.getPlaylist(functions.config().weeklyarchive.origin_playlist)
+        .then(function(data: any) {
+            // TODO: check for empty playlist
+            if (!data.body.tracks.items) {
+                console.log(data.body);
+                return;
+            }
+            spotifyApi.getPlaylist(functions.config().weeklyarchive.destination_playlist)
+            .then(function(destinationData: any) {
+                const existingUris: string[] = [];
+                if (destinationData.body.tracks.items) {
+                    for (const item of destinationData.body.tracks.items) {
+                        const uri = item.track.uri;
+                        existingUris.push(uri);
+                    }
+                }
+                
+                const uris: string[] = [];
+                if (data.body.tracks.items) {
+                    for (const item of data.body.tracks.items) {
+                        const uri = item.track.uri;
+                        if (!existingUris.includes(uri)) {
+                            uris.push(uri);
+                        }
+                    }
+                }
+                
+                spotifyApi.addTracksToPlaylist(functions.config().weeklyarchive.destination_playlist, uris, { position: 0 })
+                .then(function(addedData: any) {
+                    console.log('Added tracks to playlist!');
+                }, function(error: any) {
+                    console.log('Something went wrong while adding tracks!', error);
+                });
+            }, function(error: any) {
+                console.log('Something went wrong while fetching destination!', error);
+            });
+        }, function(error: any) {
+            console.log('Something went wrong while fetching origin!', error);
+        });
+        
+    }, function(error: any) {
+        console.log('Could not refresh access token', error);
+    });
+});
